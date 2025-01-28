f712195c83a163e1f08a942296124af0a247c8a3
Fay Ash <yaoshiu@qq.com>
2025-01-29 03:30:34 +0800

    network: change proxy port

--- system/network/default.nix
+++ system/network/default.nix
@@ -1,5 +1,5 @@
 { ... }:
 {
-  networking.proxy.default = "http://127.0.0.1:7890";
+  networking.proxy.default = "http://127.0.0.1:20122";
   networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 }
