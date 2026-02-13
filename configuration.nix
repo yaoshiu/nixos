5c438d5d063959b6bfecaf51927d98b9eab7ee22
Fay Ash <akafayash@icloud.com>
2026-02-13 18:59:15 +0800

    fix: ssh public key for gha

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID4tUJIE/YKZQTSbbewzv37957T9X6aNcvKlxDoWcALO huangyifei@MAGIT04390.local"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNLLm6m7F59XdbsiAblXyOz15kGNBmZKQ+7VoIYHm8V huangyifei@MAGIT04390.local"
   ];
 
   system.stateVersion = "25.11";
