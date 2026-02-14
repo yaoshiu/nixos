53879bbb0005fd99a5f0f42a4799c8fd4b2ac136
Fay Ash <akafayash@icloud.com>
2026-02-14 22:17:49 +0800

    fix: qemu-guest

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,9 @@
-{ ... }: {
+{ modulesPath, ... }: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+  
   boot.loader = {
     systemd-boot = {
       enable = true;
